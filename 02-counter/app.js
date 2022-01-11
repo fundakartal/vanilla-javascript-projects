a1f6e33b72f86542bbf21631dcc7b362f6415454
Funda Kartal <kartalfunda01@gmail.com>
2022-01-11 14:06:18 +0300

    Update app.js

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -8,13 +8,31 @@ const btns = document.querySelectorAll('.btn');
 btns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     const styles = e.currentTarget.classList;
+    styles.contains('decrease')
+      ? count--
+      : styles.contains('increase')
+      ? count++
+      : (count = 0);
+
+    count < 0
+      ? (value.style.color = 'red')
+      : count > 0
+      ? (value.style.color = 'green')
+      : (value.style.color = '#222');
+
+    value.textContent = count;
+
+    /* 
     if (styles.contains('decrease')) {
       count--;
     } else if (styles.contains('increase')) {
       count++;
     } else {
       count = 0;
-    }
+    } 
+    */
+
+    /* 
     if (count < 0) {
       value.style.color = 'red';
     }
@@ -23,7 +41,7 @@ btns.forEach(function (btn) {
     }
     if (count === 0) {
       value.style.color = '#222';
-    }
-    value.textContent = count;
+    } 
+    */
   });
 });
